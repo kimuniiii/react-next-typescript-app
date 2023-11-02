cb03c8b68cd4c20a3de1cf4ac39bda26d95c91e9
kimuniiii <kk.baseball.0824@gmail.com>
2023-11-02 20:31:14 +0900

    chore : .prettierrc.js ファイルを指定する

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  /** 折り返しの横幅 */
+  printWidth: 120,
+  /** オブジェクト内の要素と括弧の間にスペースを出力する */
+  bracketSpacing: true,
+  /** 複数要素の末尾の後ろにカンマを付与するかどうか */
+  trailingComma: "all",
+  /** アロー関数のパラメーターを括弧で囲むかどうか */
+  arrowParens: "always",
+};
